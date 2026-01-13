9ee53b8f2848cbb1f4efcf25bcd79922909dffc3
DaniilGorenkov <d.gorenkov@nox.ru>
2026-01-13 17:38:32 +0300

    upd

--- phd_assistant/run.py
+++ phd_assistant/run.py
@@ -5,12 +5,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # print(sys.path)
 from rag.generator.qa_pipeline import RAGPipeline
 from phd_assistant import PhDAssistant
+from datetime import datetime
+
+ts = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 rag = RAGPipeline(llm_type="local_lora")
 assistant = PhDAssistant(rag)
 
 result = assistant.generate_section_iterative(
-    topic="Искусственный интеллект и модели машинного обучения в системах диагностик", section_name="04_overview"
+    topic="Искусственный интеллект и модели машинного обучения в системах диагностик", section_name=f"{ts}_overview"
 )
 
 # print(result["draft"])
